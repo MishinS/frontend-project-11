4e741bd96b60865c0091991fe64313f5d89cad8b
sergei mishin <mishins.s@yandex.ru>
2024-01-07 13:32:07 +0300

    Validation implementation

---
+++ src/observers.js
@@ -0,0 +1,20 @@
+import onChange from 'on-change';
+
+const observers = (obj) => {
+  let index = 0;
+  const watchState = onChange(
+    obj,
+    (path, value, previousValue, applyData) => {
+      alert('value change...');
+      console.log('Object changed:', ++index);
+      console.log('this:', this);
+      console.log('path:', path);
+      console.log('value:', value);
+      console.log('previousValue:', previousValue);
+      console.log('applyData:', applyData);
+    }
+  );
+  return watchState;
+};
+
+export default observers;
